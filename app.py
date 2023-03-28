7bd8eae8fcc6246de4b8e44591903c09afadcb0e
Ramiro Berrelleza <ramiro@okteto.com>
2023-03-28 14:24:34 -0700

    use kustomize

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import os
 import pydevd_pycharm
+
 from flask import Flask
+
+import ldclient
+from ldclient import Context
+from ldclient.config import Config
 
 app = Flask(__name__)
 
+sdk_key = os.getenv("LAUNCHDARKLY_SDK_KEY")
+feature_flag_key = "use-new-logos"
+
 @app.route('/')
 def hello_world():
-    msg = 'Hello World!'
+    context = Context.builder('okteto').name('Cindy').build()
+    flag_value = ldclient.get().variation(feature_flag_key, context, False)
+    if flag_value:
+      msg = 'Hello New World! 🌎'
+    else: 
+      msg = 'Hello World! 🌐'
+    
     return msg
 
 def attach():
@@ -15,6 +29,13 @@ def attach():
     pydevd_pycharm.settrace('0.0.0.0', port=9000, stdoutToServer=True, stderrToServer=True)
 
 if __name__ == '__main__':
+  ldclient.set_config(Config(sdk_key))
+  if ldclient.get().is_initialized():
+    print("LaunchDarkly SDK successfully initialized!")
+  else:
+    print("LaunchDarkly SDK failed to initialize")
+    exit()
+
   print('Starting hello-world server...')
   # comment out to use Pycharm's remote debugger
   # attach()
